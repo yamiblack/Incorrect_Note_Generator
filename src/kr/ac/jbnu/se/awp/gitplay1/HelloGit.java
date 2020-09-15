ab2ff687a87f09954d74d34941af43e58e67a542
lazybones1 <lazybones1@naver.com>
2020-09-15 21:29:55 +0900

    JinSan

--- src/kr/ac/jbnu/se/awp/gitplay1/HelloGit.java
+++ src/kr/ac/jbnu/se/awp/gitplay1/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
 		System.out.println("Hello JooHyung");
+		System.out.println("Hello JinSan");
 	}
 }

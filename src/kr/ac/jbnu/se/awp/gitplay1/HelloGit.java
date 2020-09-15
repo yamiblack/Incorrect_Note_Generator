a231ee399fcede5668ab68211b887c292cac80a0
w00ng <56468120+JIW00NG@users.noreply.github.com>
2020-09-15 21:37:08 +0900

    JiWoong

--- src/kr/ac/jbnu/se/awp/gitplay1/HelloGit.java
+++ src/kr/ac/jbnu/se/awp/gitplay1/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
 		System.out.println("Hello Git!!");
 		System.out.println("Hello JooHyung");
 		System.out.println("Hello JinSan");
+		System.out.println("Hello JiWoong");
 	}
 }

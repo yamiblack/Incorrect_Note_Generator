73db5939acb423159e6d04cd3e9b95b3e3c9255e
JooHyung <yamiblack95@gmail.com>
2020-09-15 20:39:50 +0900

    JooHyung

--- src/kr/ac/jbnu/se/awp/gitplay1/HelloGit.java
+++ src/kr/ac/jbnu/se/awp/gitplay1/HelloGit.java
@@ -3,5 +3,6 @@ package kr.ac.jbnu.se.awp.gitplay1;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
+		System.out.println("Hello JooHyung");
 	}
 }

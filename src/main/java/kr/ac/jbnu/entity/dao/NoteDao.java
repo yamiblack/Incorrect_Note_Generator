7f3f34d973adeeec46d654eee51f44b0fd0c08dd
w00ng <56468120+JIW00NG@users.noreply.github.com>
2020-11-28 20:26:53 +0900

    수정

--- src/main/java/kr/ac/jbnu/entity/dao/NoteDao.java
+++ src/main/java/kr/ac/jbnu/entity/dao/NoteDao.java
@@ -1,5 +1,29 @@
 package kr.ac.jbnu.entity.dao;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import kr.ac.jbnu.entity.model.Note;
+import kr.ac.jbnu.entity.util.HibernateUtil;
+
 public class NoteDao {
-	
+	public void saveEmployee(Note note) {
+		System.out.println("========SAVE========");
+		Transaction transaction = null;
+		try(Session session = HibernateUtil.getSessionFactory().openSession()){
+			//start the transaction
+			transaction = session.beginTransaction();
+			
+			//save student object
+			session.save(note);
+			System.out.println("Successfully create : "+note.toString());
+			//commit the transaction
+			transaction.commit();
+			
+		}catch(Exception e) {
+			if(transaction !=null) {
+				transaction.rollback();
+			}
+		}
+	}
 }

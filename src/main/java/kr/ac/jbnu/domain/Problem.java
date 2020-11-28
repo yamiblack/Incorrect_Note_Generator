c98c653f8f2cb72980a3332884accb435a478526
jalee <jalee@DESKTOP-OKMI8JS>
2020-11-28 11:55:13 +0900

    수정

---
+++ src/main/java/kr/ac/jbnu/domain/Problem.java
@@ -0,0 +1,14 @@
+package kr.ac.jbnu.domain;
+
+public class Problem {
+	private Integer problemNum;
+	private String MemId;
+	private String problemTitle;
+	private Integer level;
+	private String category;
+	private String problem;
+	private String ans;
+	private String wrong;
+	private String describe;	
+	
+}
